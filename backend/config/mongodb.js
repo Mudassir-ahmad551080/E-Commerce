0a56c0fbd60c068ac354de814b4effc14e1ebbe4
Mudassir Ahmad <ma6386731@gmail.com>
2025-11-14 10:10:23 +0500

    another commit

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
+let isConnected = false;
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`).then(()=>{
-        console.log("MongoDB connected")
-    }).catch((err)=>{
-        console.log("Error: ",err)
-    })
-}
+const connectDB = async () => {
+    if (isConnected) return;
+
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        isConnected = true;
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.log("Mongo Error:", error);
+    }
+};
 
 export default connectDB;
